c7c0d6065a7e4fcb2aade13ac1df44893fb3006d
snusmumriki <wdart1995@gmail.com>
2018-10-19 06:53:25 -0700

    1

--- unit.c
+++ unit.c
@@ -1,1 +1,57 @@
-struct Unit {};
+#define TYPE_EYE 0
+#define TYPE_BODY 1
+#define TYPE_SKIRT 2
+
+#define CON_LEN 1.0
+
+#include <math.h>
+
+typedef struct unit {
+	char type;
+	
+	float x;
+	float y;;
+
+	float left_data;
+	float right_data;
+
+	Connector *con0;
+	Connector *con1;
+	Connector *con2;
+} Unit;
+
+typedef struct connector {
+	float length;
+	Unit *unit;
+} Connector;
+
+void unit_updateConnectors() {
+
+}
+
+void unit_normalize(Unit *unit) {
+	Unit *unit0 = unit->con0.unit;
+	Unit *unit1 = unit->con1.unit;
+
+	double a = hypot(unit1->x - unit0->x, unit1->y - unit0->y);
+	double b = unit->con0.length;
+	double c = unit->con1.length;
+	double cos_ = getCos(a, b, c);
+	double sin_ = getSin(cos_)
+	
+	unit->x = b * cos_ + left_unit->x;
+	unit->y = b * sin_ + left_unit->y;
+
+}
+
+double hypot(double a, double b) {
+	return sqrt(a*a + b*b);
+}
+
+double getCos(double a, double  b, double c) {
+	return (a*a + b*b - c*c) / ( 2.0 * a * b);
+}
+
+double getSin(double cos_) {
+	return sqrt(1.0 - cos_*cos_);
+}
