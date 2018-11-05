a90ff7c04717c74cda561a263063bfd2d8413a49
snusmumriki <wdart1995@gmail.com>
2018-11-04 16:51:40 -0800

    11

--- spgl/r_optimizer.c
+++ spgl/r_optimizer.c
@@ -1,3 +1,6 @@
+#define ROTATION_0 -1
+#define ROTATION_1 1
+
 #include <stdlib.h>
 
 #include "r_optimizer.h"
@@ -5,19 +8,19 @@
 
 #define IN(x, limit) ((x) >= 0 && (x) < limit)
 
-char **addSpace(Frame frame) {
+Pos *extPos(Frame frame, Pos init_pos, int rotation, int side) {
+	Step bs = steps[rotation];
+	bs.i = -bs.i;
+	bs.j = -bs.j;
+	Step step = steps[ROTATE(rotaton + side)];
 
-}
-
-Pos *convergent_extPos(Frame frame, Pos pos, int rotation, int side) {
-	Step bs = -step[rotation];
-	Step step = step[ROTATE(rotaton + side)];
-
-	int center = 1;	
-	Pos buffer;
+	int center = 1;
+	int convergent = 0;
+	int space = 0;
 	Pos *ext_pos = mallloc(sizeof(Pos));
+	*ext_pos = init_pos;
 
-	for (int i = pos.i, int j = pos.j; 
+	for (int i = init_pos.i, int j = init_pos.j; 
 									IN(i, frame.h) && IN(j, frame.w); 
 									i += step.i, j += step.j) {
 		char current = frame.mt[i][j];
@@ -25,40 +28,58 @@ Pos *convergent_extPos(Frame frame, Pos pos, int rotation, int side) {
 		if (current == EXT_CHAR)
 				return ext_pos;
 
-		int bi = i + bs.i;
-		int bj = i + bs.j;
-		int i_in = IN(bi, frame.h);
-		int j_in = IN(bj, frame.w);
-		int ext_line = (i_in && j_in && frame.mt[bi][bj] == SPACE_CHAR || !i_in && !j_in)
-		if (current != SPACE_CHAR && (ext_line || center))
-			buffer = {i, j};
-			
 		if (current == SPACE_CHAR) {
+			if (convergent >= 2)
+				*ext_pos = {i, j};
 			center = 0;
-			*ext_pos = buffer;
+			convergent = 0;
+		} else {
+			int bi = i + bs.i;
+			int bj = i + bs.j;
+			int bi_in = IN(bi, frame.h);
+			int bj_in = IN(bj, frame.w);
+			int ext_line = (bi_in && bj_in && frame.mt[bi][bj] == SPACE_CHAR || !bi_in && !bj_in);
+			convergent += (ext_line || center);
 		}
 	}
 
 	return ext_pos;
 }
 
-void smoothOver(Frame frame, Pos pos, Pos pos0, Pos pos1) {
+void isolate(Frame frame, Pos init_pos, Pos ext_pos, int rotation, int side) {
+	Step step = steps[rotation + side];
+
+	for (int i = init_pos.i, int j = init_pos.j; 
+									i != ext_pos.i && j != ext_pos.j; 
+									i += step.i, j += step.j) {
+		if (frame.mt[i][j] == SPACE_CHAR)
+			frame.mt[i][j] = VDEF_CHAR;
+	}
+}
+
+void fill(Frame frame, Pos init_pos) {
 	
 }
 
-void fill() {
+void optimizeLimb(Frame frame, Pos init_pos, int rotation) {
+	Step step = steps[rotation];
 
-}
-
-void optimizeLimb(Frame frame, Pos pos, int rotation) {
-	Step step = step[rotation];
-
-	for (int i = pos.i, int j = pos.j; 
+	for (int i = init_pos.i, int j = init_pos.j; 
 									IN(i, frame.h) && IN(j, frame.w); 
 									i += step.i,  j += step.j) {
-			Pos pos0 = convergent_extPos(frame, pos, rotation, -1);
-			Pos pos1 = convergent_extPos(frame, pos, rotation, 1);
-			smoothOver(frame, pos, pos0, pos1);
+			Pos pos = {i, j};
+
+			Pos ext_pos0 = extPos(frame, pos, rotation, ROTATION_0);
+			Pos ext_pos1 = extPos(frame, pos, rotation, ROTATION_1);
+			isolate(frame, pos, ext_pos0, rotation, ROTATION_0);
+			isolate(frame, pos, ext_pos1, rotation, ROTATION_1)
 		}
 }
 
+char optimizeFrame(Frame *frame) {
+
+}
+
+void optimizeRobot(Frame frame) {
+	
+}
